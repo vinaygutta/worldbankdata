022a590443881c54dbf1ff4e3caeca5aa2ff1ef1
Vinay Babu <vinaygutta@yahoo.com>
2016-07-07 11:00:03 +0000

    commit2

--- WorldBankData/src/com/test/worldbankdata/WorldBankData.java
+++ WorldBankData/src/com/test/worldbankdata/WorldBankData.java
@@ -8,6 +8,7 @@ import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -29,7 +30,9 @@ public class WorldBankData {
 	    driver = new FirefoxDriver();
 	    	    baseUrl = "http://www.worldbank.org";
 	    	    driver.manage().window().maximize();
-	    //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    	    Dimension dim=new Dimension(800,600);
+	    	    driver.manage().window().setSize(dim); 
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	    	    webwait=new WebDriverWait(driver,20);
 	  }
 
@@ -41,7 +44,22 @@ public class WorldBankData {
 	    driver.findElement(By.linkText("By Country")).click();
 	    //isElementPresent(By.linkText("High income"));
 	    //webwait.until(ExpectedConditions.elementToBeClickable(By.linkText("High income")));
-	    driver.findElement(By.linkText("High income")).click();
+	    Thread.sleep(10000);
+	    String highincome = driver.findElement(By.linkText("High income")).getAttribute("href");
+	    driver.get(highincome);
+	    //isElementPresent(By.linkText("Andorra"));
+	    /*System.out.println("beforeelement");
+	    WebElement element = driver.findElement(By.linkText("Andorra"));
+System.out.println("afterelement");
+	    Actions actions = new Actions(driver);
+	    System.out.println("afteractiona");
+	    actions.moveToElement(element).click().perform();
+	    System.out.println("aftermove");*/
+	    String unitst = driver.findElement(By.linkText("United States")).getAttribute("href");
+	    driver.get(unitst);
+	    
+	    //driver.findElement(By.linkText("United States")).click();
+
 	    //driver.findElement(By.linkText("Andorra")).click();
 	    // ERROR: Caught exception [unknown command [clickAndWaitre]]
 	    //driver.findElement(By.linkText("High income")).click();
