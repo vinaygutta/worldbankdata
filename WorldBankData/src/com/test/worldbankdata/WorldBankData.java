1cbe197442ff25bfa61a9acae7357bbc16a686b2
vin <vin@vinpc>
2016-07-08 14:07:59 +0530

    completedandworking

--- WorldBankData/src/com/test/worldbankdata/WorldBankData.java
+++ WorldBankData/src/com/test/worldbankdata/WorldBankData.java
@@ -1,106 +1,248 @@
 package com.test.worldbankdata;
 
-import java.util.regex.Pattern;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WorldBankData {
-	
+
 	static String driverPath = "C:\\Program Files (x86)\\Internet Explorer\\iexplore.exe";
 
+	private HashMap<String, Long> mapgdp = new HashMap<String, Long>();
+	private HashMap<String, Long> mappopu = new HashMap<String, Long>();
+	private HashMap<String, Long> mapco2 = new HashMap<String, Long>();
+
+	FileWriter writergdp = null;
+	FileWriter writerpopu = null;
+	FileWriter writerco2 = null;
+
+	long startTime;
+	long endTime;
+
 	private WebDriver driver;
-	  private String baseUrl;
-	  private boolean acceptNextAlert = true;
-	  private StringBuffer verificationErrors = new StringBuffer();
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
 
-	  @Before
-	  public void setUp() throws Exception {
-		  System.setProperty("webdriver.ie.driver", driverPath);
-	    //driver = new InternetExplorerDriver();
-	    driver = new FirefoxDriver();
-	    	    baseUrl = "http://www.worldbank.org";
-	    	    driver.manage().window().maximize();
-//	    	    Dimension dim=new Dimension(800,600);
-//	    	    driver.manage().window().setSize(dim); 
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	  }
+	private WorldBankDataUtilities utiliy;
 
-	  @Test
-	  public void testScriptext() throws Exception {
-	    driver.get("http://www.worldbank.org");
-	    driver.findElement(By.xpath("(//a[contains(text(),'Data')])[2]")).click();
-	    driver.findElement(By.linkText("visit the old site here")).click();
-	    driver.findElement(By.linkText("By Country")).click();
-	    
-	    String highincome = driver.findElement(By.linkText("High income")).getAttribute("href");
-	    driver.get(highincome);
+	@Before
+	public void setUp() throws Exception {
+		// System.setProperty("webdriver.ie.driver", driverPath);
+		// driver = new InternetExplorerDriver();
+		driver = new FirefoxDriver();
+		baseUrl = "http://www.worldbank.org";
+		driver.manage().window().maximize();
+		// Dimension dim=new Dimension(800,600);
+		// driver.manage().window().setSize(dim);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		utiliy = new WorldBankDataUtilities();
+		startTime = System.currentTimeMillis();
+	}
 
-	    String unitst = driver.findElement(By.linkText("Sweden")).getAttribute("href");
-	    driver.get(unitst);
-	    
-	    String gdp = driver.findElement(By.xpath("//*[@id='nav-header-top-left-area']/div[2]/div/div/div[1]/div/div[2]/span/span/a/span")).getText();
-	    System.out.println("GDP: " + gdp);
-	    
-	    String str = gdp.replaceAll("[^-?.0-9]+", "");
-	    
-	    double gdpint = Double.parseDouble(str);
-	    System.out.println("GDP Int = " +gdpint);
-	    
-	    
-	    String popu = driver.findElement(By.xpath("//*[@id='nav-header-top-left-area']/div[2]/div/div/div[2]/div/div/div/div[2]/span/span/a/span")).getText();
-	    System.out.println("Population: " + popu);	    
-	    
-	  }
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testScriptext() throws Exception {
 
-	  @After
-	  public void tearDown() throws Exception {
-	    driver.quit();
-	    String verificationErrorString = verificationErrors.toString();
-	    if (!"".equals(verificationErrorString)) {
-	      fail(verificationErrorString);
-	    }
-	  }
+		String cntry = "";
+		String gdp = "";
+		String popu = "";
+		String co2 = "";
+		driver.get(baseUrl);
+		driver.findElement(By.xpath("(//a[contains(text(),'Data')])[2]")).click();
+		driver.findElement(By.linkText("visit the old site here")).click();
+		driver.findElement(By.linkText("By Country")).click();
 
-	  private boolean isElementPresent(By by) {
-	    try {
-	    	System.out.println(by);
-	      driver.findElement(by);
-	      return true;
-	    } catch (NoSuchElementException e) {
-	      return false;
-	    }
-	  }
+		String highincome = driver.findElement(By.linkText("High income")).getAttribute("href");
+		driver.get(highincome);
 
-	  private boolean isAlertPresent() {
-	    try {
-	      driver.switchTo().alert();
-	      return true;
-	    } catch (NoAlertPresentException e) {
-	      return false;
-	    }
-	  }
+		String countrylist[] = utiliy.getCountList();
+		utiliy.generateCsvFileHeader();
 
-	  private String closeAlertAndGetItsText() {
-	    try {
-	      Alert alert = driver.switchTo().alert();
-	      String alertText = alert.getText();
-	      if (acceptNextAlert) {
-	        alert.accept();
-	      } else {
-	        alert.dismiss();
-	      }
-	      return alertText;
-	    } finally {
-	      acceptNextAlert = true;
-	    }
-	  }
+		for (String country : countrylist) {
+			cntry = "";
+			gdp = "";
+			popu = "";
+			co2 = "";
+			if (isElementPresent(By.linkText(country))) {
+				String unitst = driver.findElement(By.linkText(country)).getAttribute("href");
+				driver.get(unitst);
+			}
+
+			if (isElementPresent(By.xpath("//*[@id='nav-header-top-left-area']/h1"))) {
+				cntry = driver.findElement(By.xpath("//*[@id='nav-header-top-left-area']/h1")).getText();
+				// System.out.println("=============");
+				// System.out.println(cntry);
+			}
+
+			if (isElementPresent(By
+					.xpath("//*[@id='nav-header-top-left-area']/div[2]/div/div/div[1]/div/div[2]/span/span/a/span"))) {
+				gdp = driver
+						.findElement(By
+								.xpath("//*[@id='nav-header-top-left-area']/div[2]/div/div/div[1]/div/div[2]/span/span/a/span"))
+						.getText();
+
+				// System.out.println("GDP: " + gdp);
+				long gdplong = utiliy.convertStringToNumber(gdp);
+
+				// System.out.println("GDP Long: " + gdplong);
+
+				mapgdp.put(cntry, gdplong);
+				// System.out.println("GDP Int = " +gdpint);
+			}
+
+			if (isElementPresent(By.xpath(
+					"//*[@id='nav-header-top-left-area']/div[2]/div/div/div[2]/div/div/div/div[2]/span/span/a/span"))) {
+				popu = driver
+						.findElement(By
+								.xpath("//*[@id='nav-header-top-left-area']/div[2]/div/div/div[2]/div/div/div/div[2]/span/span/a/span"))
+						.getText();
+				// System.out.println("Population: " + popu);
+				long populong = utiliy.convertStringToNumber(popu);
+
+				// System.out.println("Popu Long: " + populong);
+				mappopu.put(cntry, populong);
+			}
+
+			if (isElementPresent(By.xpath("//*[@id='boxes-box-country_wdi_block2']/div/div[2]/div/div/div/span[1]"))) {
+				co2 = driver
+						.findElement(By.xpath("//*[@id='boxes-box-country_wdi_block2']/div/div[2]/div/div/div/span[1]"))
+						.getText();
+				// System.out.println("CO2: " + co2);
+				long co2long = utiliy.convertStringToNumber(co2);
+
+				// System.out.println("CO2 Long: " + co2long);
+				mapco2.put(cntry, co2long);
+			}
+
+			utiliy.generateCsvFile(cntry.replaceAll("[,]+", ""), gdp.replaceAll("[,]+", ""),
+					popu.replaceAll("[,]+", ""), co2.replaceAll("[,]+", ""));
+
+			driver.get(highincome);
+		}
+		Object[] agdp = mapgdp.entrySet().toArray();
+		Arrays.sort(agdp, new Comparator() {
+			public int compare(Object o1, Object o2) {
+				return ((Map.Entry<String, Long>) o2).getValue().compareTo(((Map.Entry<String, Long>) o1).getValue());
+			}
+		});
+		int count = 1;
+		try {
+			writergdp = new FileWriter("top3countriesbygdp.log");
+			writergdp.flush();
+			writergdp.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		for (Object e : agdp) {
+			// System.out.println(((Map.Entry<String, Long>) e).getKey() + " : "
+			// + ((Map.Entry<String, Long>) e).getValue());
+			try {
+				if (count <= 3) {
+					writergdp = new FileWriter("top3countriesbygdp.log", true);
+					writergdp.append(
+							((Map.Entry<String, Long>) e).getKey() + " : " + ((Map.Entry<String, Long>) e).getValue());
+					writergdp.append("\n");
+					writergdp.flush();
+					writergdp.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+			count++;
+		}
+
+		Object[] apopu = mappopu.entrySet().toArray();
+		Arrays.sort(apopu, new Comparator() {
+			public int compare(Object o1, Object o2) {
+				return ((Map.Entry<String, Long>) o2).getValue().compareTo(((Map.Entry<String, Long>) o1).getValue());
+			}
+		});
+		int countpopu = 1;
+		try {
+			writerpopu = new FileWriter("top3countriesbypopu.log");
+			writerpopu.flush();
+			writerpopu.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		for (Object e : apopu) {
+			// System.out.println(((Map.Entry<String, Long>) e).getKey() + " : "
+			// + ((Map.Entry<String, Long>) e).getValue());
+			try {
+				if (countpopu <= 3) {
+					writerpopu = new FileWriter("top3countriesbypopu.log", true);
+					writerpopu.append(
+							((Map.Entry<String, Long>) e).getKey() + " : " + ((Map.Entry<String, Long>) e).getValue());
+					writerpopu.append("\n");
+					writerpopu.flush();
+					writerpopu.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+			countpopu++;
+		}
+
+		Object[] aco2 = mapco2.entrySet().toArray();
+		Arrays.sort(aco2, new Comparator() {
+			public int compare(Object o1, Object o2) {
+				return ((Map.Entry<String, Long>) o2).getValue().compareTo(((Map.Entry<String, Long>) o1).getValue());
+			}
+		});
+		int countco2 = 1;
+		try {
+			writerco2 = new FileWriter("top3countriesbyco2.log");
+			writerco2.flush();
+			writerco2.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		for (Object e : aco2) {
+			// System.out.println(((Map.Entry<String, Long>) e).getKey() + " : "
+			// + ((Map.Entry<String, Long>) e).getValue());
+			try {
+				if (countco2 <= 3) {
+					writerco2 = new FileWriter("top3countriesbyco2.log", true);
+					writerco2.append(
+							((Map.Entry<String, Long>) e).getKey() + " : " + ((Map.Entry<String, Long>) e).getValue());
+					writerco2.append("\n");
+					writerco2.flush();
+					writerco2.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+			countco2++;
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		endTime = System.currentTimeMillis();
+		long elapsedTime = (endTime - startTime);
+
+		long diffSeconds = elapsedTime / 1000 % 60;
+		long diffMinutes = elapsedTime / (60 * 1000) % 60;
+		long diffHours = elapsedTime / (60 * 60 * 1000);
+
+		System.out.println("Run Time: " + diffHours + " Hours " + diffMinutes + " Minutes " + diffSeconds + " Seconds");
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
 }
---
+++ WorldBankData/src/com/test/worldbankdata/WorldBankDataUtilities.java
@@ -0,0 +1,144 @@
+package com.test.worldbankdata;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WorldBankDataUtilities {
+	private String[] countList = { "Andorra", "Antigua and Barbuda", "Aruba", "Australia", "Austria", "Bahamas, The",
+			"Bahrain", "Barbados", "Belgium", "Bermuda", "British Virgin Islands", "Brunei Darussalam", "Canada",
+			"Cayman Islands", "Channel Islands", "Chile", "Croatia", "Curacao", "Cyprus", "Czech Republic", "Denmark",
+			"Estonia", "Faroe Islands", "Finland", "France", "French Polynesia", "Germany", "Gibraltar", "Greece",
+			"Greenland", "Guam", "Hong Kong SAR, China", "Hungary", "Iceland", "Ireland", "Isle of Man", "Israel",
+			"Italy", "Japan", "Korea, Rep.", "Kuwait", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg",
+			"Macao SAR, China", "Malta", "Monaco", "Nauru", "Netherlands", "New Caledonia", "New Zealand",
+			"Northern Mariana Islands", "Norway", "Oman", "Poland", "Portugal", "Puerto Rico", "Qatar", "San Marino",
+			"Saudi Arabia", "Seychelles", "Singapore", "Sint Maarten (Dutch part)", "Slovak Republic", "Slovenia",
+			"Spain", "St. Kitts and Nevis", "St. Martin (French part)", "Sweden", "Switzerland", "Taiwan, China",
+			"Trinidad and Tobago", "Turks and Caicos Islands", "United Arab Emirates", "United Kingdom",
+			"United States", "Uruguay", "Virgin Islands (U.S.)" };
+	// private String[] countList = {"Andorra","Antigua and Barbuda","Bahamas,
+	// The","Australia"};
+
+	private final String COMMA_DELIMITER = ",";
+	private final String NEW_LINE_SEPARATOR = "\n";
+
+	// CSV file header
+	private final String FILE_HEADER = "Country,GDP,Population,CO2";
+	FileWriter writer = null;
+
+	public WorldBankDataUtilities() {
+	}
+
+	public String[] getCountList() {
+		return countList;
+	}
+
+	public void generateCsvFileHeader() {
+		try {
+			writer = new FileWriter("countrydata.csv");
+
+			writer.append(FILE_HEADER.toString());
+
+			// Add a new line separator after the header
+			writer.append(NEW_LINE_SEPARATOR);
+
+			// generate whatever data you want
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+
+			try {
+				writer.flush();
+				writer.close();
+			} catch (IOException e) {
+				System.out.println("Error while flushing/closing fileWriter !!!");
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	public void generateCsvFile(String country, String gdp, String population, String co2) {
+		try {
+			writer = new FileWriter("countrydata.csv", true);
+
+			writer.append(country);
+			writer.append(COMMA_DELIMITER);
+			writer.append(gdp);
+			writer.append(COMMA_DELIMITER);
+			writer.append(population);
+			writer.append(COMMA_DELIMITER);
+			writer.append(co2);
+			writer.append(NEW_LINE_SEPARATOR);
+
+			// generate whatever data you want
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+
+			try {
+				writer.flush();
+				writer.close();
+			} catch (IOException e) {
+				System.out.println("Error while flushing/closing fileWriter !!!");
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	public void generateCsvFile(String country, int gdp, int population, int co2) {
+		try {
+			writer = new FileWriter("countrydata.csv", true);
+
+			writer.append(country);
+			writer.append(COMMA_DELIMITER);
+			writer.append(Integer.toString(gdp));
+			writer.append(COMMA_DELIMITER);
+			writer.append(Integer.toString(population));
+			writer.append(COMMA_DELIMITER);
+			writer.append(Integer.toString(co2));
+			writer.append(NEW_LINE_SEPARATOR);
+
+			// generate whatever data you want
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+
+			try {
+				writer.flush();
+				writer.close();
+			} catch (IOException e) {
+				System.out.println("Error while flushing/closing fileWriter !!!");
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	public long convertStringToNumber(String str) {
+		long gdpint = 0;
+		String strtemp = str.replaceAll("[^-?.0-9]+", "");
+
+		double gdpdouble = Double.parseDouble(strtemp);
+
+		if (str.contains("million")) {
+			gdpint = (long) (gdpdouble * 1_000_000);
+		} else if (str.contains("billion")) {
+			gdpint = (long) (gdpdouble * 1_000_000 * 1000);
+		} else if (str.contains("trillion")) {
+			gdpint = (long) (gdpdouble * 1_000_000 * 1_000_000);
+		} else {
+			gdpint = (long) gdpdouble;
+		}
+
+		return gdpint;
+	}
+
+}
